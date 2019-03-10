b432317c16556630d0838a8fdb77f5985992439b
Lukasz <ywnwa96@gmail.com>
2019-03-10 19:04:21 +0100

    Updates

---
+++ precache-manifest.75139ff79ab6a418fa2099a4ff4a99b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4258d1a182c64027fb",
+    "url": "/gitrending/static/js/runtime~main.4fedef85.js"
+  },
+  {
+    "revision": "8072ad28324a62f825c8",
+    "url": "/gitrending/static/js/main.aa97c3b1.chunk.js"
+  },
+  {
+    "revision": "7b060a3908ca9255d0a5",
+    "url": "/gitrending/static/js/2.d4a0c6df.chunk.js"
+  },
+  {
+    "revision": "4626299697b47ca2b66acd2214dac2e8",
+    "url": "/gitrending/index.html"
+  }
+];
